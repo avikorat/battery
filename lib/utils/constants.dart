78676ab14694670abedabf5e8cf2179d5a280f1a
Avinash <patelavi955@gmail.com>
2023-04-17 14:47:39 -0400

    commit:           - parameters are corrected.

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,6 +4,6 @@ const String SETUP = "setUp";
 var BOX;
 bool notification = false;
 
-String PowerSonic = "C:0:4;C:1:0;C:2:51;C:3:14.6;C:4:5;C:50:74.6;C:55:0";
-String Discover = "C:0:2;C:1:0;C:2:51;C:3:14.2;C:4:5;C:50:72.2;C:55:0";
-String RitarPower = "C:0:1;C:1:0;C:2:51;C:3:14.4;C:4:5;C:50:71.4;C:55:0";
+String PowerSonic = "C:0:4;C:1:0;C:2:51;C:3:1460;C:4:500;C:50:2015;C:55:0";
+String Discover = "C:0:2;C:1:0;C:2:51;C:3:1420;C:4:500;C:50:1973;C:55:0";
+String RitarPower = "C:0:3;C:1:0;C:2:51;C:3:1440;C:4:500;C:50:1994;C:55:0";
